b570d4807cc89448869286dd738fae92daaf7df8
David Zhao (DEVDIV) <dazhao@microsoft.com>
2016-08-05 08:41:57 -0700

    add buildtasks

---
+++ DockerTools2/DockerTools2.BuildTasks/DockerComposeBuild.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Utilities;
+using System;
+
+namespace DockerTools2.BuildTasks
+{
+    public class DockerComposeBuild : Task
+    {
+        public override bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
